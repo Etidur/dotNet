4d9e28e114edd2180b86006350803a5a5ed6182e
Admin <Admin@DESKTOP-P14QAEA>
2018-10-10 09:08:00 +0200

    init modif v2

---
+++ ClientConvertisseurV2-bis/ClientConvertisseurV2/View/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using ClientConvertisseurV2.Model;
+using ClientConvertisseurV2.Service;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ClientConvertisseurV2.View
+{
+    /// <summary>
+    /// Initialise l'interface graphique
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ ClientConvertisseurV2-bis/ClientConvertisseurV2/ViewModel/MainViewModel.cs
@@ -0,0 +1,105 @@
+﻿using ClientConvertisseurV2.Model;
+using ClientConvertisseurV2.Service;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Popups;
+
+namespace ClientConvertisseurV2.ViewModel
+{
+    /// <summary>
+    /// Classe faisant le lien entre le modèle et la vue
+    /// </summary>
+    public class DevToEuroViewModel : ViewModelBase
+    {
+        private ObservableCollection<Devise> _comboBoxDevises;
+        public ICommand BtnSetConversion { get; private set; }
+
+        private string _montantEuros;
+        private Devise _devise;
+        private string _montantConverti;
+
+        public ObservableCollection<Devise> ComboBoxDevises
+        {
+            get { return _comboBoxDevises; }
+            set
+            {
+                _comboBoxDevises = value;
+                RaisePropertyChanged();// Pour notifier de la modification de ses donnée
+            }
+        }
+        public string MontantEuros {
+            get { return _montantEuros; }
+            set { _montantEuros = value; RaisePropertyChanged(); }
+        }
+        public Devise ComboBoxDeviseItem
+        {
+            get { return _devise; }
+            set { _devise = value; RaisePropertyChanged(); }
+        }
+        public string MontantConverti
+        {
+            get { return _montantConverti; }
+            set { _montantConverti = value; RaisePropertyChanged("MontantConverti"); }
+        }
+
+        /// <summary>
+        /// Complète l'interface avec des valeurs dynamiques et initialise l'action du bouton
+        /// </summary>
+        public DevToEuroViewModel()
+        {
+            ActionGetData();
+            BtnSetConversion = new RelayCommand(ActionSetConversion);
+        }
+
+        /// <summary>
+        /// Calcule la conversion et affiche le résultat dans l'interface
+        /// </summary>
+        private void ActionSetConversion()
+        {
+            double montant = 0, taux = 1;
+
+            try
+            {
+                montant = Convert.ToDouble(_montantEuros);
+                taux = ComboBoxDeviseItem.Taux;
+            }
+            catch (FormatException)
+            {
+                this.MessageBox("Le montant n'est pas correct");
+            }
+            catch (NullReferenceException)
+            {
+                this.MessageBox("Aucune devise n'a été séléctionnée");
+            }
+
+            MontantConverti = Convert.ToString(montant * taux);
+         }
+
+        /// <summary>
+        /// Récupère la liste des devises via le service connecté à l'API
+        /// </summary>
+        private async void ActionGetData()
+        {
+            WSService connection = WSService.GetInstance();
+            var result = await connection.GetAllDevisesAsync("Devise");
+            this.ComboBoxDevises = new ObservableCollection<Devise>(result);
+        }
+
+        /// <summary>
+        /// Afficher un message dans une boîte de dialogue
+        /// </summary>
+        /// <param name="message">Message (String) à afficher</param>
+        private async void MessageBox(string message)
+        {
+            var dialog = new MessageDialog(message);
+            await dialog.ShowAsync();
+        }
+    }
+}
